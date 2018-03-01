5971f8fda1e31573a5f1f8bdd6c60ef39cb85afa
Alexander Kostenko <a.kostenko@cwi.nl>
2018-03-01 18:50:40 +0100

    work in progress
    rk in progree

--- flexbox/flexCompute.py
+++ flexbox/flexCompute.py
@@ -8,10 +8,85 @@ This module contains calculation routines for pre/post processing.
 """
 import numpy
 from scipy import ndimage
+from scipy import signal
+
 
 from . import flexUtil
 from . import flexData
 from . import flexProject
+
+def remove_dark(data):
+    '''
+    Thresholds the data below the first minimum in the histogram
+    '''
+    # Obtain the first minima on the histogram - assuming this is the border between air and material
+    x, y = histogram(data)
+    y = ndimage.filters.gaussian_filter1d(y, sigma=1)
+
+    extremum = signal.argrelextrema(y, numpy.less)[0]
+    
+    # Zero the intensity below extrema:
+    data[data < x[extremum[0]]] = 0
+
+    return data
+
+def itk_register(fixed, moving):
+    '''
+    Registration of two 3D volumes.
+    
+    return:
+    '''
+    
+    import SimpleITK as sitk
+    
+    #read the images
+    fixed_image =  sitk.GetImageFromArray(fixed)
+    moving_image = sitk.GetImageFromArray(moving)
+
+    # Center:
+    initial_transform = sitk.CenteredTransformInitializer(fixed_image, 
+                                                      moving_image, 
+                                                      sitk.Euler3DTransform(), 
+                                                      sitk.CenteredTransformInitializerFilter.GEOMETRY)
+    
+    # Regitration:
+    registration_method = sitk.ImageRegistrationMethod()
+    
+    # Similarity metric settings.
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01)
+    
+    registration_method.SetInterpolator(sitk.sitkLinear)
+    
+    # Optimizer settings.
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100, convergenceMinimumValue=1e-6, convergenceWindowSize=10)
+    registration_method.SetOptimizerScalesFromPhysicalShift()
+    
+    # Setup for the multi-resolution framework.            
+    registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4,2,1])
+    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2,1,0])
+    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+    
+    # Don't optimize in-place, we would possibly like to run this cell multiple times.
+    registration_method.SetInitialTransform(initial_transform, inPlace=False)
+    
+    # Connect all of the observers so that we can perform plotting during registration.
+    #registration_method.AddCommand(sitk.sitkStartEvent, start_plot)
+    #registration_method.AddCommand(sitk.sitkEndEvent, end_plot)
+    #registration_method.AddCommand(sitk.sitkMultiResolutionIterationEvent, update_multires_iterations) 
+    #registration_method.AddCommand(sitk.sitkIterationEvent, lambda: plot_values(registration_method))
+    
+    final_transform = registration_method.Execute(sitk.Cast(fixed_image, sitk.sitkFloat32), 
+                                                  sitk.Cast(moving_image, sitk.sitkFloat32))
+
+    print('Final metric value: {0}'.format(registration_method.GetMetricValue()))
+    print('Optimizer\'s stopping condition, {0}'.format(registration_method.GetOptimizerStopConditionDescription()))
+
+    moving = sitk.Resample(moving_image, fixed_image, final_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+
+    #interact(display_images_with_alpha, image_z=(0,fixed_image.GetSize()[2]), alpha=(0.0,1.0,0.05), fixed = fixed(fixed_image), moving=fixed(moving_resampled));
+    return final_transform
 
 def rotate(data, angle, axis = 0):
     '''
@@ -70,7 +145,8 @@ def histogram(data, nbin = 256, plot = True, log = False):
     # Set bin values to the middle of the bin:
     x = (x[0:-1] + x[1:]) / 2
 
-    flexUtil.plot(x, y, semilogy = True, title = 'Histogram')
+    if plot:
+        flexUtil.plot(x, y, semilogy = log, title = 'Histogram')
     
     return x, y
     
