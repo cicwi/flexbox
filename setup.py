696ce379c3f0c1cf4de297ace96c9f449ff9c017
Allard Hendriksen <allard.hendriksen@cwi.nl>
2018-08-15 13:41:51 +0200

    Add imageio to dependencies again
    The imageio package seems to install with pip without problems.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     "scikit-image>=0.13.0",
     #"matplotlib>=2.0.0", # these are problematic packages somehow...
     #"transforms3d",
-    #"imageio>=2.2.0",
+    "imageio>=2.2.0",
     #"astra-toolbox>1.8.3",
     #"xraylib>=3.3.0",   # xraylib's conda package isn't found by setuptools
     "toml>=0.9.0"],
